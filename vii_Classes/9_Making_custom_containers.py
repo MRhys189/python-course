4a8fa94872eb7ea009f3249ca617824eec772cfe
McRhys189 <muragerhys189@gmail.com>
2020-02-06 18:41:44 +0300

    new lessoms

---
+++ vii_Classes/9_Making_custom_containers.py
@@ -0,0 +1,28 @@
+# a class that keeps track of various tags in a blog
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
+
+    def add(self, tag):
+        self.tags[tag] = self.tags.get(tag.lower, 0) + 1
+
+    def __getitem__(self, tag):
+        return self.tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.tags)
+
+    def __iter__(self):
+        return iter(self.tags)
+
+
+cloud = TagCloud()
+
+cloud["python"]
+cloud.add("Python")
+cloud.add("python")
+cloud.add("python")
+print(cloud.tags)
