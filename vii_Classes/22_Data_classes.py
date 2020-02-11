bb6a5a5899400a1d25f254f22d32401483b187e7
McRhys189 <muragerhys189@gmail.com>
2020-02-11 14:14:50 +0300

    Importing from other modules

--- vii_Classes/22_Data_classes.py
+++ vii_Classes/22_Data_classes.py
@@ -1,12 +1,6 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+from collections import namedtuple
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-
-p1 = Point(1, 2)
+Point = namedtuple("Point", ["x","y"])
+p1 = Point(x=1, y=2)
 p2 = Point(1, 2)
 print(p1 == p2)
