ff3c3aa312113a3edc74695a82ddc2485d866092
McRhys189 <muragerhys189@gmail.com>
2020-02-11 11:01:37 +0300

    Non-pythonic with 6

--- vii_Classes/duck_typing.py
+++ vii_Classes/duck_typing.py
@@ -1,13 +1,13 @@
-my_list = [1, 2, 3, 4, 5]
+my_list = [1, 2, 3, 4, 5, 6]
 
 # Non-pythonic
-# if len(my_list) >= 6:
-#     print(my_list[5])
-# else:
-#     print("That index does not exist")
+if len(my_list) >= 6:
+    print(my_list[5])
+else:
+    print("That index does not exist")
 
 # Pythonic
-try:
-    print(my_list[5])
-except IndexError:
-    print("That index does not exist")
+# try:
+#     print(my_list[5])
+# except IndexError:
+#     print("That index does not exist")
