36979a817574b914c2f7a7725f0705f0af0bf8be
McRhys189 <muragerhys189@gmail.com>
2020-04-23 23:59:36 +0300

    review

---
+++ iv_Functions/fb.py
@@ -0,0 +1,13 @@
+def fizz_buzz(input):
+    if (input % 5) == 0 and (input % 3) == 0:
+        return "FizzBuzz"
+    else:
+        if (input % 5) == 0:
+            return "Buzz"
+        elif (input % 3) == 0:
+            return "Fizz"
+        else:
+            return input
+
+
+print(fizz_buzz(input=21))
